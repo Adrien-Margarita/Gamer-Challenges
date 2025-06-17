782ac2756efc1ae1ee2cb4740229c34008c1793d
Dominique DEBERT <dominiquedebert@gmail.com>
2025-06-17 13:46:54 +0200

    Feat/ Game Detail Page

--- project/frontend/src/pages/ChallengeDetailPage.tsx
+++ project/frontend/src/pages/ChallengeDetailPage.tsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router";
 
 export default function ChallengeDetailPage() {
     const { id } = useParams<{ id: string }>();
-    const { data: challenge, isLoading, isError } = useChallenge(id ?? "");
+    const { data: challenge, isLoading } = useChallenge(id ?? "");
     console.log(challenge)
 
   if (isLoading) {
@@ -23,33 +23,37 @@ export default function ChallengeDetailPage() {
     );
   }
     return (
-        <div className="min-h-screen flex flex-col bg-gradient-to-r from-[#12243E] to-[#314C6B]">
-            {/* Navbar */}
-            <Navbar />
-            <main className="flex-1 p-4 space-y-12">
-                <section>
-                    {isLoading && <p className="text-center">Chargement...</p>}
+      <div className="min-h-screen flex flex-col bg-gradient-to-r from-[#12243E] to-[#314C6B]">
+          {/* Navbar */}
+          <Navbar />
+          <main className="flex-1 p-4 space-y-12">
+              <section>
+                  {isLoading && <p className="text-center">Chargement...</p>}
 
-                    {!isLoading && challenge && (
-                   <>
-                    {/* Image et description du challenge */}
-                        <div className="relative w-full max-w-7xl mx-auto mb-12">
-                            <img
-                            src={challenge.image_url}
-                            alt={challenge.title}
-                            className="w-full h-[500px] object-cover rounded-xl shadow-xl"
-                            />
-                            <div className="absolute bottom-4 left-6 px-4 py-2 rounded-xl backdrop-blur-sm bg-base-200 ">
-                                <h2 className="text-2xl font-semibold text-white">{challenge.title}</h2>
-                            <p className="text-sm text-gray-300">{challenge.description}</p>
-                            </div>
-                        </div>
-                    </>
-                    )}
-    
-                </section>
-              </main>
-            <Footer />
-        </div>
+                  {!isLoading && challenge && (
+                  <>
+                  {/* Image et description du challenge */}
+                    <div className="w-full max-w-7xl mx-auto mb-12">
+                      <img
+                      src={challenge.image_url}
+                      alt={challenge.title}
+                      className="w-full h-[180px] object-cover border border-primary
+                      transition-transform transition-filter duration-300 ease-in-out
+                      filter hover:grayscale hover:contrast-100 transform scale-100 hover:scale-110"
+                      draggable="false"
+                      loading="lazy"
+                      />
+                      <div className="absolute bottom-4 left-6 px-4 py-2 rounded-xl backdrop-blur-sm bg-base-200 ">
+                        <h2 className="text-2xl font-semibold text-white">{challenge.title}</h2>
+                        <p className="text-sm text-gray-300">{challenge.description}</p>
+                      </div>
+                    </div>
+                  </>
+                  )}
+  
+              </section>
+            </main>
+          <Footer />
+      </div>
     );
 }
