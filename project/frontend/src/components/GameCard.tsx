b83c5585b3bb6ff1e8ad3fc77e5b23caabcdde6f
Dominique DEBERT <dominiquedebert@gmail.com>
2025-06-18 15:46:25 +0200

    feat/ Gamecard design improvements

--- project/frontend/src/components/GameCard.tsx
+++ project/frontend/src/components/GameCard.tsx
@@ -14,12 +14,9 @@ export default function GameCard({ game }: { game: IGame }) {
         loading="lazy"
       />
 
-      <div className="absolute bottom-4 left-2 px-4 py-2 rounded-xl backdrop-blur-sm transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+      <div className="absolute bottom-0 left-0 w-full px-4 py-2 transition-opacity duration-300 bg-[#12243E] opacity-0 group-hover:opacity-100">
         <h3 className="text-lg font-semibold text-white">{game.title}</h3>
       </div>
-      <div className="absolute top-4 right-4 text-white text-2xl font-bold">
-        {[28, 12, 38, 52, 15, 3, 9, 44][Number(game.game_id)]}
-      </div> 
     </div>
   );
 }
